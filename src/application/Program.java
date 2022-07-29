4d296e6571c1f6eae53a22f46fc5e6de588df06e
Tawan Silva <tawan.tls43@gmail.com>
2022-07-29 19:13:41 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import modal.dao.DaoFactory;
+import modal.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,10 +13,12 @@ public class Program {
 		
 		
 		Department obj = new Department(1, "Books"); 
-		System.out.println(obj);
 		
 		
 		Seller seller = new Seller(21, "Tawan", "tawan.tls43@gmail.com", new Date(), 6000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 
